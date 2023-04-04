abf2d40efa043aca380a843c4f381e3a24d1cc62
olenaweb <olena.nevzorova@gmail.com>
2023-04-04 19:53:45 +0300

    feat: burger menu done (Tue, Apr 04, 2023 07:53:44 PM)

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -1,12 +1,31 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const burgerList = document.querySelector('.burger-list');
-// const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 const headerBar = document.querySelector('.header-bar');
+const body = document.querySelector('body');
 
-function menuToggle1() {
+function menuToggle() {
   console.log(burgerMenu.classList);
   burgerMenu.classList.toggle('active');
   burgerMenu.classList.toggle('inactive');
   headerBar.classList.toggle('change');
+  if (burgerMenu.classList.contains('active')) {
+    modal.style.display = "block";
+    body.style.overflow = 'hidden';
+  } else {
+    modal.style.display = "none";
+    body.style.overflow = 'auto';
+  }
 }
 
+burgerMenu.addEventListener('click', (event) => {
+  console.log('event.target = ', event.target);
+  menuToggle();
+});
+
+window.onclick = function (event) {
+  console.log('event.target = ', event.target);
+  if (event.target == modal) {
+    menuToggle();
+  }
+}
