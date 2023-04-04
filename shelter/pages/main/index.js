0dda6ed69bbaab56a550fa71dd9eae98e4723aaa
olenaweb <olena.nevzorova@gmail.com>
2023-04-05 01:03:08 +0300

    fix: card modified (Wed, Apr 05, 2023 01:03:06 AM)

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -29,3 +29,7 @@ window.onclick = function (event) {
     menuToggle();
   }
 }
+
+
+
+
--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,10 +1,13 @@
 "use strict";
 import shelter from "../../assets/json/animal.js";
+console.log('animal = ', shelter);
+for (let pet of shelter) {
+  console.log('pet.name = ', pet.name.toLowerCase(), pet);
+}
 
 let template = document.querySelector("#template");
-
 document.addEventListener("DOMContentLoaded", function () {
-  for (let i = 0; i < shelter.length; i++) {
+  for (let i = 0; i < 3; i++) {
     loadContent(shelter[i]);
     let card = document.querySelectorAll(".card")
     card[i].dataset.pet = shelter[i].name;
@@ -21,4 +24,3 @@ function loadContent() {
   let itemCarousel = document.querySelector("#item-active");
   itemCarousel.append(templateClone);
 }
-export default loadContent;
